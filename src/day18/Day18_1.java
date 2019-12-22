85c46d0a91da4a04f219ba8d2806a39d71a5bf40
uriel.zylbermann@gmail.com <uriel.zylbermann@gmail.com>
2019-12-22 16:03:46 +0200

    Refactor 18.1. Failing

--- src/day18/Day18_1.java
+++ src/day18/Day18_1.java
@@ -12,13 +12,7 @@ public class Day18_1 {
     public static void main(String[] args) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(
-                    //"C:\\Users\\UrielZ\\Documents\\Workspaces\\Advent of code\\Advent\\src\\day18\\test inpu 1.txt"
-                    "C:\\Users\\נטע\\Documents\\אוריאל\\projects\\Advent Of Code\\AdventOfCode\\src\\day18\\test input 1.txt"
-                    // "C:\\Users\\נטע\\Documents\\אוריאל\\projects\\Advent Of Code\\AdventOfCode\\src\\day18\\test input 2.txt"
-                    // "C:\\Users\\נטע\\Documents\\אוריאל\\projects\\Advent Of Code\\AdventOfCode\\src\\day18\\test input 3.txt"
-                    // "C:\\Users\\נטע\\Documents\\אוריאל\\projects\\Advent Of Code\\AdventOfCode\\src\\day18\\test input 4.txt"
-                    // "C:\\Users\\נטע\\Documents\\אוריאל\\projects\\Advent Of Code\\AdventOfCode\\src\\day18\\test input 5.txt"
-                    // "C:\\Users\\נטע\\Documents\\אוריאל\\projects\\Advent Of Code\\AdventOfCode\\src\\day18\\puzzle input.txt"
+                    "C:\\Users\\UrielZ\\Documents\\Workspaces\\Advent of code\\Advent\\src\\day18\\test input 1.txt"
                     ));
             TunnelNavigator navigator = new TunnelNavigator(reader.lines().collect(Collectors.toList()));
             System.out.println(navigator.getShortestPathToAllKeys());
--- src/day18/TunnelNavigator.java
+++ src/day18/TunnelNavigator.java
@@ -11,22 +11,24 @@ public class TunnelNavigator {
 
     private static final int MAX = Integer.MAX_VALUE / 2;
     int height, width;
-    char[][] grid;
-    int[][][][] distances = new int[0][0][0][0];
-    Blockers[][][][] blockers = new Blockers[0][0][0][0];
+    Map<Pair<Integer, Integer>, Character> grid = new HashMap<>();
+    Map<Pair<Integer, Integer>, Map<Pair<Integer, Integer>, Integer>> distances;
+    Map<Pair<Integer, Integer>, Map<Pair<Integer, Integer>, Blockers>> blockers;
     Map<Character, Pair<Integer, Integer>> specialLocations = new HashMap<>();
 
 	public TunnelNavigator(List<String> input) {
         height = input.size();
         width = input.get(0).length();
-        grid = new char[height][width];
+        char temp;
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                grid[i][j] = input.get(i).charAt(j);
-                specialLocations.put(grid[i][j], Pair.with(i, j));
+                temp = input.get(i).charAt(j);
+                if (temp != '#') {
+					grid.put(Pair.with(i,  j), temp);
+	                specialLocations.put(temp, Pair.with(i, j));
+				}
             }
         }
-        specialLocations.remove('#');
         specialLocations.remove('.');
 	}
 
@@ -39,90 +41,67 @@ public class TunnelNavigator {
     private void initPaths() {
         int x, y;
         Blockers doors;
-        distances = new int[height][width][height][width];
-        blockers = new Blockers[height][width][height][width];
+        distances = new HashMap<>();
+        blockers = new HashMap<>();
         List<Pair<Integer, Integer>> directions = 
                 Arrays.asList(
                     Pair.with(1, 0), 
                     Pair.with(0, 1));
-
-        initAsMax();
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 for (Pair<Integer,Integer> direction : directions) {
                     x = i + direction.getValue0();
                     y = j + direction.getValue1();
-                    if (hasValue(i, j, x, y)) {
+                    if (hasValues(i, j, x, y)) {
                         doors = checkBlockers(i, j, x, y);
-                        distances[i][j][x][y] = distances[x][y][i][j] = 1;
-                        blockers[i][j][x][y] = new Blockers().add(doors);
-                        blockers[x][y][i][j] = new Blockers().add(doors);
+                        distances.getOrDefault(Pair.with(i, j), new HashMap<>())
+                    		.put(Pair.with(x, y), 1);
+                        distances.getOrDefault(Pair.with(x, y), new HashMap<>())
+                    		.put(Pair.with(i, j), 1);
+                        blockers.getOrDefault(Pair.with(i, j), new HashMap<>())
+                    		.put(Pair.with(x, y), new Blockers().add(doors));
+                        blockers.getOrDefault(Pair.with(x, y), new HashMap<>())
+                    		.put(Pair.with(i, j), new Blockers().add(doors));
                     }
                 }
+                distances.getOrDefault(Pair.with(i, j), new HashMap<>())
+                	.put(Pair.with(i, j), 0);
             }
         }
     }
 
-    private void initAsMax() {
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                for (int k = 0; k < height; k++) {
-                    for (int l = 0; l < width; l++) {
-                        distances[i][j][k][l] = MAX;
-                    }
-                }
-            }
-        }
-        
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                distances[i][j][i][j] = 0;
-                blockers[i][j][i][j] = new Blockers();
-            }
-        }
-    }
-
-    private boolean hasValue(int i, int j, int x, int y) {
-        if (x < 0 || y < 0 || x >= height || y >= width) {
-            return false;
-        }
-        if (grid[i][j] == '#' || grid[x][y] == '#') {
-            return false;
-        }
-        return true;
+    private boolean hasValues(int i, int j, int x, int y) {
+        return grid.containsKey(Pair.with(i, j)) && grid.containsKey(Pair.with(x, y));
     }
 
     private Blockers checkBlockers(int i, int j, int x, int y) {
         Blockers doors = new Blockers();
-        if (grid[i][j] >= 'A' && grid[i][j] <= 'B') {
-            doors.add(grid[i][j]);
+        if (grid(i, j) >= 'A' && grid(i, j) <= 'Z') {
+            doors.add(grid(i, j));
         }
-        if (grid[x][y] >= 'A' && grid[x][y] <= 'B') {
-            doors.add(grid[x][y]);
+        if (grid(x, y) >= 'A' && grid(x, y) <= 'Z') {
+            doors.add(grid(x, y));
         }
         return doors;
     }
 
-    private void floydWarshall() {
-        for (int x = 0; x < height; x++) {
-            for (int y = 0; y < width; y++) {
-                for (int i = 0; i < height; i++) {
-                    for (int j = 0; j < width; j++) {
-                        for (int k = 0; k < height; k++) {
-                            for (int l = 0; l < width; l++) {
-                                if (distances[i][j][x][y] + distances[x][y][k][l] < distances[i][j][k][l]) {
-                                    distances[i][j][k][l] = distances[i][j][x][y] + distances[x][y][k][l];
-                                    blockers[i][j][k][l] = new Blockers()
-                                        .add(blockers[i][j][x][y])
-                                        .add(blockers[x][y][k][l]);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+	private void floydWarshall() {
+		for (Pair<Integer, Integer> i : grid.keySet()) {
+			for (Pair<Integer, Integer> j : grid.keySet()) {
+				for (Pair<Integer, Integer> k : grid.keySet()) {
+					if (getDistance(j, i) + getDistance(i, k) < getDistance(j, k)){
+						distances.getOrDefault(j, new HashMap<>())
+							.put(k, distances.get(j).get(i) + distances.get(i).get(k));
+						blockers.getOrDefault(j, new HashMap<>())
+							.put(k, 
+								new Blockers()
+								.add(blockers.get(j).get(i))
+								.add(blockers.get(i).get(k)));
+					}
+				}	
+			}	
+		}
     }
 
     private int getShortestPath() {
@@ -139,11 +118,22 @@ public class TunnelNavigator {
                         + "\tl2: "
                         + l2.toString()
                         + "\tDistance: "
-                        + distances[l1.getValue0()][l1.getValue1()][l2.getValue0()][l2.getValue1()]
+                        + getDistance(l1, l2)
                         + "\tBlockers: "
-                        + blockers[l1.getValue0()][l1.getValue1()][l2.getValue0()][l2.getValue1()].toString()
+                        + getBlocker(l1, l2)
                         + "\t")));
         return 0;
     }
 
+	private char grid(int i, int j) {
+    	return grid.get(Pair.with(i, j));
+	}
+
+	private Integer getDistance(Pair<Integer, Integer> i, Pair<Integer, Integer> j) {
+		return distances.getOrDefault(i, new HashMap<>()).getOrDefault(j, MAX);
+	}
+
+    private Blockers getBlocker(Pair<Integer, Integer> i, Pair<Integer, Integer> j) {
+		return blockers.getOrDefault(i, new HashMap<>()).getOrDefault(j, new Blockers());
+	}
 }
