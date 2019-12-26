3aaf38a626880ef1dc3ae445bd9152da81318865
uriel.zylbermann@gmail.com <uriel.zylbermann@gmail.com>
2019-12-26 13:46:14 +0200

    23.2

--- src/day23/NetworkController.java
+++ src/day23/NetworkController.java
@@ -2,11 +2,16 @@ package day23;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Queue;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import org.javatuples.Pair;
 
 import intcode.IntcodeMachine;
 
@@ -22,13 +27,39 @@ public class NetworkController {
         this.program = program;
 	}
 
-	public int runMachines() {
+	public long runMachines() {
 		
         initMachines();
         runUntilNATPackage();
         
         input.poll();
-        return input.poll().intValue();
+        return input.poll();
+	}
+	
+
+	public long runMachinesWithNAT() {
+		
+		Pair<Long, Long> currentNATPacket = Pair.with(null, null), previousNATPacket;
+		Long lastYSentByNAP = null;
+		
+        initMachines();
+        
+        while (true) {
+            runUntilNATPackage();
+            previousNATPacket = currentNATPacket;
+            currentNATPacket = Pair.with(input.poll(), input.poll());
+            if (currentNATPacket.toList().stream().allMatch(Objects::isNull)) {
+            	currentNATPacket = previousNATPacket;
+            	if (lastYSentByNAP == currentNATPacket.getValue1()) {
+					return lastYSentByNAP;
+				}
+            	lastYSentByNAP = currentNATPacket.getValue1();
+				input.add(0L);
+				input.add(currentNATPacket.getValue0());
+				input.add(currentNATPacket.getValue1());
+			}
+		}
+        
 	}
 
 	private void initMachines() {
