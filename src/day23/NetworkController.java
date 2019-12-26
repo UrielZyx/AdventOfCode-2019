9621cfdc420631bceaee59b3a2044b206c71511d
uriel.zylbermann@gmail.com <uriel.zylbermann@gmail.com>
2019-12-26 12:06:28 +0200

    Refactor 23.1 to get rid of threading

--- src/day23/NetworkController.java
+++ src/day23/NetworkController.java
@@ -1,14 +1,11 @@
 package day23;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Map;
+import java.util.List;
 import java.util.Queue;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import intcode.IntcodeMachine;
@@ -16,32 +13,64 @@ import intcode.IntcodeMachine;
 public class NetworkController {
 
     final static int NUMBER_OF_MACHINES = 50;
-    Map<Integer, Boolean> initialized = new ConcurrentHashMap<>();
-    Map<Integer ,Queue<Long>> inputs = new ConcurrentHashMap<>();
-    Map<Integer ,Thread> threads = new ConcurrentHashMap<>();
+    Queue<Long> input = new LinkedList<>();
+    List<IntcodeMachine> machines = new ArrayList<>();
+    
     String program;
 
 	public NetworkController(String program) {
         this.program = program;
 	}
 
-	public void runMachines() {
-
-		ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_MACHINES);
+	public int runMachines() {
+		
+        initMachines();
+        runUntilNATPackage();
         
-        IntStream.range(0, NUMBER_OF_MACHINES)
-            .forEach(i -> inputs.put(i, new LinkedList<>()));
+        input.poll();
+        return input.poll().intValue();
+	}
 
-        IntStream.range(0, NUMBER_OF_MACHINES)
-            .forEach(i -> {
-            	executor.execute(() -> 
-                				new IntcodeMachine(program)
-									.setInputIterator(new NetworkInputIterator(this, i))
-				                    .setOutputHandler(new NetworkOutputHandler(this))
-				                    .runProgram());
-            });
-        
-        executor.shutdown();
+	private void initMachines() {
+		IntStream.range(0, NUMBER_OF_MACHINES)
+        	.forEach(i -> {
+        		machines.add(new IntcodeMachine(program)
+									.addInput(Arrays.asList(i))
+									.setOutputHandler(input::add));
+        	});
+	}
+
+	private void runUntilNATPackage() {
+		int address;
+		runAllMachinesWithNoInput();
+		
+		while(!input.isEmpty()) {
+			if (runMachineWithNextInput()) {
+				break;
+			}
+			runAllMachinesWithNoInput();			
+		}
+	}
+
+	private boolean runMachineWithNextInput() {
+		int address;
+		address = input.poll().intValue();
+		System.out.println("Machine: " + address);
+		if (address < NUMBER_OF_MACHINES) {
+			machines.get(address)
+					.addInput(Arrays.asList(input.poll(), input.poll()))
+					.runProgram();
+			return false;
+		}
+		return true;
+	}
+
+	private void runAllMachinesWithNoInput() {
+		for (int i = 0; i < NUMBER_OF_MACHINES && input.isEmpty(); i++) {
+			machines.get(i)
+					.addInput(Arrays.asList(-1))
+					.runProgram();
+		}
 	}
 
 }
